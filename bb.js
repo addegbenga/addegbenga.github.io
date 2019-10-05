c2a68bcae569b61e7c58051267588fd408894ea8
adeyemi gbenga <addegbenga@gmail.com>
2019-10-05 01:44:00 +0100

    uploads

---
+++ bb.js
@@ -0,0 +1,11 @@
+const droplinks = document.querySelector(".drops");
+const links = document.querySelectorAll("drop1");
+const tap = document.querySelector(".clickk");
+const b = document.querySelector(".b");
+const c = document.querySelector(".c");
+
+tap.addEventListener("click", function() {
+
+    droplinks.classList.toggle("open");
+
+});
